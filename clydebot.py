06cee5a65a5219b171754d3d5e6691c69314a110
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2019-05-22 16:58:35 +0600

    Update clydebot.py

--- clydebot.py
+++ clydebot.py
@@ -13,8 +13,7 @@ def is_owner(ctx):
     return ctx.message.author.id == "488353416599306270"
 
 @bot.command(pass_context = True)
-@commands.check(is_owner)     
-async def bal():
+async def bal(ctx):
     embed=discord.Embed(title="ADIB HOQUE's Balance:",description='You currently have 10,084,069 credits.',color=0x00AE86)
     embed.set_thumbnail(url= 'https://cdn.discordapp.com/attachments/554964608130088980/580696234680123403/PokecordMoney.png')
     await bot.say(embed=embed)
